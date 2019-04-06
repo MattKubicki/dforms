84d08f648a30fe5aa876c6319284407c018d1709
MattKubicki <mateuszkubicki@op.pl>
2019-04-06 15:26:19 +0200

    built database

--- dforms/forms/models.py
+++ dforms/forms/models.py
@@ -11,30 +11,30 @@ class Form(models.Model):
     owner_id = models.ForeignKey(Users, on_delete=models.CASCADE)
 
 
-class Questions(models.Model):
+class Question(models.Model):
     question_text = models.CharField(max_length=100)
     is_open_question = False
     form_id = models.ForeignKey(Form, on_delete=models.CASCADE)
-# id chyyyba kmini samo sadzac po przykładach
 
     def __str__(self):
         return self.question_text
 
 
+class Answers(models.Model):
+    user_id = models.ForeignKey(Users, on_delete=models.CASCADE)
+
+
 class Choice(models.Model):  # to do pytan zamknietych
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
     choice_text = models.CharField(max_length=200)
+    answer_id = models.ForeignKey(Answers, on_delete=models.CASCADE)
 
     def __str__(self):
         return self.choice_text
 
 
 class OpenQuestionAnswers(models.Model):
-    question = models.ForeignKey(Questions, on_delete=models.CASCADE)
-    user = models.ForeignKey(Questions, on_delete=models.CASCADE)
+    question = models.ForeignKey(Question, on_delete=models.CASCADE)
+    answer_id = models.ForeignKey(Answers, on_delete=models.CASCADE)
+    answer_text = models.CharField(max_length=200)
 
-    choice_text = models.CharField(max_length=200)
-
-
-class Answers(models.Model):
-    pass
